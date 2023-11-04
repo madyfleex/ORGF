78206869a9d67f26a140b04b23aaf4fe6df8b562
madyfleexGT <89738692+madyfleex@users.noreply.github.com>
2023-11-04 01:46:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,45 +1,36 @@
-//Dependencies
-const Request = require("request")
+//Dependencies const axios = require(‘axios’)
 
-//Variables
-const Self_Args = process.argv.slice(2)
+//Variables const Self_Args = process.argv.slice(2)
 
-//Main
-if(!Self_Args.length){
-    console.log("https://discord.com/api/webhooks/1169722670833750016/j2cjvJ1E3ipQdtIau60IPRc6R9DFrfUCVg48QjJSbi1SZ0fIsebnt-t7eMlMJIDt2PG2>")
-    process.exit()
-}
+//Main if(!Self_Args.length){ console.log(‘node index.js <webhook_link>’) process.exit() }
 
-if(!Self_Args[0]){
-    console.log("Invalid webhook_link.")
-    process.exit()
-}
+if(!Self_Args[0]){ console.log(‘Invalid webhook_link.’) process.exit() }
 
-setInterval(function(){
-    const group_id = Math.floor(Math.random() * 200000 * 5150000)
+setImmediate(function loop(){ const group_id = Math.floor(Math.random() * 1150000)
+                             axios.get(`https://groups.roblox.com/v1/groups/${group_id}`)
+.then(function(response){
+    const body = response.data
 
-    Request(`https://groups.roblox.com/v1/groups/${group_id}`, function(err, res, body){
-        if(err){
-            console.log(`[ORGF][Invalid] https://www.roblox.com/groups/group.aspx?gid=${group_id}`)
-            return
-        }
+    if(!body.owner && body.publicEntryAllowed){
+        axios.post(Self_Args[0], {
+            content: `[ORGF][Valid] https://www.roblox.com/groups/group.aspx?gid=${group_id} | Name: ${body.name} | Members: ${body.memberCount}`
+        })
+        .then(function(response){
+            console.log(`[ORGF][Valid] https://www.roblox.com/groups/group.aspx?gid=${group_id} | Name: ${body.name} | Members: ${body.memberCount}`)
+        })
+        .catch(function(error){
+            console.log(error)
+        })
+        
+        return
+    }else{
+        console.log(`[ORGF][Invalid] https://www.roblox.com/groups/group.aspx?gid=${group_id} | Name: ${body.name} | Members: ${body.memberCount}`)
+        return
+    }
+})
+.catch(function(error){
+    console.log(`[ORGF][Invalid] https://www.roblox.com/groups/group.aspx?gid=${group_id}`)
+    return
+})
 
-        body = JSON.parse(body)
-
-        if(!body.owner && body.publicEntryAllowed){
-            Request.post(Self_Args[0], {
-                headers: {
-                    "Content-Type": "application/x-www-form-urlencoded"
-                },
-                body: `content=[ORGF][Valid] https://www.roblox.com/groups/group.aspx?gid=${group_id} | Name: ${body.name} | Members: ${body.memberCount}`
-            }, function(err, res, body){
-                console.log(`[ORGF][Valid] https://www.roblox.com/groups/group.aspx?gid=${group_id} | Name: ${body.name} | Members: ${body.memberCount}`)
-            })
-            
-            return
-        }else{
-            console.log(`[ORGF][Invalid] https://www.roblox.com/groups/group.aspx?gid=${group_id} | Name: ${body.name} | Members: ${body.memberCount}`)
-            return
-        }
-    })
-}, 1000)
+setImmediate(loop)
