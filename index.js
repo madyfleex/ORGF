2bf050467d7f6358c1db4d246b70653e5d218019
madyfleexGT <89738692+madyfleex@users.noreply.github.com>
2023-11-04 01:12:22 -0400

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ if(!Self_Args[0]){
 }
 
 setInterval(function(){
-    const group_id = Math.floor(Math.random() (3000000 * 4000000)
+    const group_id = Math.floor(Math.random() * (3000000 * 4000000))
 
     Request(`https://groups.roblox.com/v1/groups/${group_id}`, function(err, res, body){
         if(err){
