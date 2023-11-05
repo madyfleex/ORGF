f7b66c9316e69b41727164765da207376a02a7be
madyfleexGT <89738692+madyfleex@users.noreply.github.com>
2023-11-05 12:25:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,39 @@
-import os
-import threading
-import requests, random;
-from dhooks
-import Webhook
-webhook = 'https://discord.com/api/webhooks/1167933381196140544/v4T2b9Nye_bTQqn53_ob1bbigDh9hY-UIOciZizyR6J_0MJWoYgnz4ODKEwgplRbIo8x'
-MIN_GROUP_ID => 300000
-MAX_GROUP_ID => 320000
+import os 
+import module_name as threading
+import requests as rq
+from module_name import random
+from dhooks import Webhook # Importar la clase Webhook del módulo dhooks
+    webhook = "https://discord.com/api/webhooks/1167933381196140544/v4T2b9Nye_bTQqn53_ob1bbigDh9hY-UIOciZizyR6J_0MJWoYgnz4ODKEwgplRbIo8x"
 
-def get_group_ids():
-	for group_id in range(MIN_GROUP_ID, MAX_GROUP_ID + 1):
-	yield group_id
-def find_open_groups(): open_groups = [group_id
-	for group_id in get_group_ids() if is_open_group(group_id)
-]
-if open_groups: content = "Encontré los siguientes grupos abiertos:\n{0}".format("\n".join(f 'https://www.roblox.com/groups/{i}'
-	for i in open_groups))
-requests.post(webhook, data = {
-	"content": content
-}) print(f "Encontré {len(open_groups)} grupos abiertos en el rango de {MIN_GROUP_ID} a {MAX_GROUP_ID}")# Función auxiliar para verificar si un grupo es abierto
+MIN_GROUP_ID => 300000
+MAX_GROUP_ID => 350000
+
+def get_group_ids(): for group_id in range (MIN_GROUP_ID, MAX_GROUP_ID + 1):
+		yield group_id
+
+# Función para encontrar los grupos abiertos
+def find_open_groups():
+	open_groups = [group_id for group_id in get_group_ids() if is_open_group(group_id)]
+	if open_groups:
+		content = "Encontré los siguientes grupos abiertos:\n{0}".format("\n".join(f'https://www.roblox.com/groups/{i}' for i in open_groups))
+		rq.post(webhook, data = {
+			"content": content
+		})
+		print(f"Encontré {len(open_groups)} grupos abiertos en el rango de {MIN_GROUP_ID} a {MAX_GROUP_ID}")
+
+# Función auxiliar para verificar si un grupo es abierto
 def is_open_group(group_id):
-	try: group = requests.get(f "https://groups.roblox.com/v1/groups/{group_id}") group_data = group.json()
-return group_data.get('publicEntryAllowed', False) and group_data['owner'] is None except requests.exceptions.HTTPError as e: print(f "Error al obtener el grupo {group_id}: {e.response.status_code}")
-except requests.exceptions.ConnectionError as e: print(f "Error al obtener el grupo {group_id}: {e.request.url}")
-except requests.exceptions.RequestException as e: print(f "Error al obtener el grupo {group_id}: {e}"
+	try:
+		group = rq.get(f"https://groups.roblox.com/v1/groups/{group_id}")
+		group_data = group.json()
+		return group_data.get('publicEntryAllowed', False) and group_data['owner'] is None
+	except rq.exceptions.HTTPError as e:
+		print(f"Error al obtener el grupo {group_id}: {e.response.status_code}")
+	except rq.exceptions.ConnectionError as e:
+		print(f"Error al obtener el grupo {group_id}: {e.request.url}")
+	except rq.exceptions.RequestException as e:
+		print(f"Error al obtener el grupo {group_id}: {e}")
 		return False
 
-		# Ejecutar la función principal find_open_groups()
+# Ejecutar la función principal find_open_groups()
+find_open_groups()
